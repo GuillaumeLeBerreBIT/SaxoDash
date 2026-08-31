898639786e56c6ab00951ad70619f7d26f47f5a9
GuillaumeLeBerreBIT <leberreguillaume.glb@gmail.com>
2026-08-31 23:07:44 +0200

    fix: correct accounts URL name typo (acount-list -> account-list)

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,8 +1,7 @@
-
-from django.urls import path, include
+from django.urls import path
 from .views import BankAccountListView, NetWorthView
 
 urlpatterns = [
-    path('', BankAccountListView.as_view(), name=('acount-list')),
-    path('net-worth/', NetWorthView.as_view(), name=('net-worth')),
+    path('', BankAccountListView.as_view(), name='account-list'),
+    path('net-worth/', NetWorthView.as_view(), name='net-worth'),
 ]
